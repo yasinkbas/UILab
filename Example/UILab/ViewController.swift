b0e08d918747e819486c351d50d8b6c830999798
yasin <yasin.kbas12@gmail.com>
2020-02-16 16:12:20 +0300

    update example project - minor refactor on pod - added new small design

--- Example/UILab/ViewController.swift
+++ Example/UILab/ViewController.swift
@@ -6,45 +6,124 @@
 //  Copyright (c) 2020 yasinkbas. All rights reserved.
 //
 
+// Explanation //
+// (1)
+// (2)
+// (3)
+// (4)
+// (5)
+
 import UILab
 
 class ViewController: UIViewController {
+    
+    // MARK: - set view objects basically
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.black
+        label.font = label.font.withSize(16)
+        label.textAlignment = .center
+        label.text = "UILab"
+        label.verboseName = "my_title_label"
+        return label
+    }()
     
     lazy var yellowView: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.yellow
         view.layer.shadowColor = UIColor.black.cgColor
+        view.verboseName = "my_yellow_view"
         return view
     }()
     
     lazy var clickButton: UIButton = {
         let button = UIButton()
-        button.setTitle("UILab", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.backgroundColor = UIColor.yellow
-        button.verboseName = "my_yellow_button"
+        button.setTitle("Click button", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor.clear
+        button.layer.cornerRadius = 10
+        button.verboseName = "my_yellow_button" // (1)
         button.addTarget(self, action: #selector(clickedButton(_:)), for: .touchUpInside)
         return button
     }()
     
+    // MARK: - loadView
     override func loadView() {
         super.loadView()
-        view.backgroundColor = UIColor.white
+        view.backgroundColor = UIColor.darkGray
         
         view.addSubview(yellowView)
+        view.addSubview(titleLabel)
         view.addSubview(clickButton)
-        yellowView.set(.left(view.left, 0), .right(view.right, 0), .top(view.top, 0), .height(80))
-        clickButton.set(.center(view), .width(200), .height(50))
+        
+        setConstraints()
     }
     
+    // MARK: - setup initial constraints
+    func setConstraints() {
+        // (2)
+        yellowView.set(
+            .left(view.left),
+            .right(view.right),
+            .top(view.top),
+            .height(80)
+        )
+        
+        // (3)
+        titleLabel.set(
+            .bottom(yellowView.bottom, 8),
+            .left(yellowView.left),
+            .right(yellowView.right)
+        )
+        
+        // (4)
+        clickButton.set(
+            .center(view),
+            .width(200),
+            .height(50)
+        )
+    }
+    
+    // MARK: - button target
     @objc
     func clickedButton(_ sender: UIButton) {
-        let height = yellowView.get(.height).first
-        let to = self.view.bounds.height * 6 / 10
+        let height = yellowView.get(.height).first // (5)
+        let to = self.view.bounds.height * 8 / 10
         height?.constant = height?.constant == to ? 80 : to
+        
+        let clicked = height?.constant == to
         
         UIView.animate(withDuration: 1) {
             self.view.layoutIfNeeded()
+            self.titleLabel.transform = clicked ?
+                CGAffineTransform(scaleX: 2, y: 2)
+                    .concatenating(CGAffineTransform(translationX: 0, y: 100)) : .identity
         }
+        
+        
+        UIView.transition(with: titleLabel, duration: 0.2, options: .transitionFlipFromTop, animations: {
+            self.titleLabel.textColor = clicked ? UIColor.white : UIColor.black
+        })
+        
+        UIView.transition(with: clickButton, duration: 0.2, options: .transitionFlipFromTop, animations: {
+            clicked ?
+                self.clickButton.setTitleColor(UIColor.black, for: .normal) :
+                self.clickButton.setTitleColor(UIColor.white, for: .normal)
+        })
+        
+        if clicked {
+            self.yellowView.roundCorners([.bottomLeft, .bottomRight], radius: 200)
+        }
+    }
+}
+
+// MARK: - extension
+extension UIView {
+    func roundCorners(_ corners:UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
+        self.layer.masksToBounds = true
     }
 }
--- UILab/Classes/Constraints/ConstraintSet.swift
+++ UILab/Classes/Constraints/ConstraintSet.swift
@@ -14,7 +14,7 @@ extension UIView {
         var _assign1: Any? = nil
         var _assign2: Any? = nil
         
-        func logPoint(_ anchor: Any?, _ assign1: Any?, _ assign2: Any?, constraint: Constraint?) {
+        func setPoint(_ anchor: Any?, _ assign1: Any?, _ assign2: Any?, constraint: Constraint?) {
             _anchor = anchor
             _assign1 = assign1
             _assign2 = assign2
@@ -32,57 +32,57 @@ extension UIView {
             switch $0 {
             case .left(let anchor, let inset):
                 self.leftAnchor.constraint(equalTo: anchor, constant: inset).isActive = true
-                logPoint(anchor, inset, nil, constraint: self.constraints.last)
+                setPoint(anchor, inset, nil, constraint: self.constraints.last)
                 
             case .right(let anchor, let inset):
                 self.rightAnchor.constraint(equalTo: anchor, constant: -inset).isActive = true
-                logPoint(anchor, inset, nil, constraint: self.constraints.last)
+                setPoint(anchor, inset, nil, constraint: self.constraints.last)
                 
             case .top(let anchor, let inset):
                 self.topAnchor.constraint(equalTo: anchor, constant: inset).isActive = true
-                logPoint(anchor, inset, nil, constraint: self.constraints.last)
+                setPoint(anchor, inset, nil, constraint: self.constraints.last)
                 
             case .bottom(let anchor, let inset):
                 self.bottomAnchor.constraint(equalTo: anchor, constant: -inset).isActive = true
-                logPoint(anchor, inset, nil, constraint: self.constraints.last)
+                setPoint(anchor, inset, nil, constraint: self.constraints.last)
                 
             case .leading(let anchor, let inset):
                 self.leading.constraint(equalTo: anchor, constant: inset).isActive = true
-                logPoint(anchor, inset, nil, constraint: self.constraints.last)
+                setPoint(anchor, inset, nil, constraint: self.constraints.last)
                 
             case .trailing(let anchor, let inset):
                 self.trailing.constraint(equalTo: anchor, constant: inset).isActive = true
-                logPoint(anchor, inset, nil, constraint: self.constraints.last)
+                setPoint(anchor, inset, nil, constraint: self.constraints.last)
                 
             case .width(let width):
                 self.widthAnchor.constraint(equalToConstant: width).isActive = true
-                logPoint(nil, width, nil, constraint: self.constraints.last)
+                setPoint(nil, width, nil, constraint: self.constraints.last)
                 
             case .height(let height):
                 self.heightAnchor.constraint(equalToConstant: height).isActive = true
-                logPoint(nil, height, nil, constraint: self.constraints.last)
+                setPoint(nil, height, nil, constraint: self.constraints.last)
                 
             case .centerX(let anchor, let inset):
                 self.centerXAnchor.constraint(equalTo: anchor, constant: inset).isActive = true
-                logPoint(anchor, inset, nil, constraint: self.constraints.last)
+                setPoint(anchor, inset, nil, constraint: self.constraints.last)
                 
             case .centerY(let anchor, let inset):
                 self.centerYAnchor.constraint(equalTo: anchor, constant: inset).isActive = true
-                logPoint(anchor, inset, nil, constraint: self.constraints.last)
+                setPoint(anchor, inset, nil, constraint: self.constraints.last)
                 
             case .widthDimension(let dimension, let multiple, let inset):
                 self.width.constraint(equalTo: dimension, multiplier: multiple, constant: inset).isActive = true
-                logPoint(dimension, multiple, inset, constraint: self.constraints.last)
+                setPoint(dimension, multiple, inset, constraint: self.constraints.last)
                 
             case .heightDimension(let dimension, let multiple, let inset):
                 self.height.constraint(equalTo: dimension, multiplier: multiple, constant: inset).isActive = true
-                logPoint(dimension, multiple, inset, constraint: self.constraints.last)
+                setPoint(dimension, multiple, inset, constraint: self.constraints.last)
                 
             case .center(let view, x: let xInset, y: let yInset):
                 self.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: xInset).isActive = true
-                logPoint(view, xInset, yInset, constraint: self.constraints.last)
+                setPoint(view, xInset, yInset, constraint: self.constraints.last)
                 self.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: yInset).isActive = true
-                logPoint(view, xInset, yInset, constraint: self.constraints.last)
+                setPoint(view, xInset, yInset, constraint: self.constraints.last)
             }
         }
     }
