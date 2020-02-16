424cfce6b9897383cde307616e50589b09c6d0b3
yasin <yasin.kbas12@gmail.com>
2020-02-16 18:26:19 +0300

    minor code refactor example project
    - design changed
    
    - added centerX & centerY attributes
    
    - foler names refactored

--- Example/UILab/ViewController.swift
+++ Example/UILab/ViewController.swift
@@ -17,6 +17,9 @@ import UILab
 
 class ViewController: UIViewController {
     
+    lazy var thinBarWidth: CGFloat = self.view.bounds.width / 2 - 10
+    lazy var thinBarHeight: CGFloat = 1
+    
     // MARK: - set view objects basically
     lazy var titleLabel: UILabel = {
         let label = UILabel()
@@ -24,15 +27,19 @@ class ViewController: UIViewController {
         label.font = label.font.withSize(16)
         label.textAlignment = .center
         label.text = "UILab"
-        label.verboseName = "my_title_label"
+        label.verboseName = "my_title_label" // (1)
         return label
     }()
     
     lazy var yellowView: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.yellow
-        view.layer.shadowColor = UIColor.black.cgColor
-        view.verboseName = "my_yellow_view"
+        view.layer.shadowColor = UIColor.darkGray.cgColor
+        view.layer.shadowOpacity = 1
+        view.layer.shadowOffset = .zero
+        view.layer.shadowRadius = 10
+        view.layer.shouldRasterize = false
+        view.verboseName = "my_yellow_view" // (1)
         return view
     }()
     
@@ -47,14 +54,22 @@ class ViewController: UIViewController {
         return button
     }()
     
+    lazy var thinWhiteBar: UIView = {
+        let bar = UIView()
+        bar.backgroundColor = UIColor.white
+        bar.verboseName = "my_thin_white_bar" // (1)
+        return bar
+    }()
+    
     // MARK: - loadView
     override func loadView() {
         super.loadView()
-        view.backgroundColor = UIColor.darkGray
+        view.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
         
         view.addSubview(yellowView)
         view.addSubview(titleLabel)
         view.addSubview(clickButton)
+        view.addSubview(thinWhiteBar)
         
         setConstraints()
     }
@@ -82,12 +97,20 @@ class ViewController: UIViewController {
             .width(200),
             .height(50)
         )
+        
+        // (5)
+        thinWhiteBar.set(
+            .top(yellowView.bottom, 16),
+            .width(thinBarWidth),
+            .height(thinBarHeight),
+            .centerX(yellowView.centerX)
+        )
     }
     
     // MARK: - button target
     @objc
     func clickedButton(_ sender: UIButton) {
-        let height = yellowView.get(.height).first // (5)
+        let height = yellowView.get(.height).first // (6)
         let to = self.view.bounds.height * 8 / 10
         height?.constant = height?.constant == to ? 80 : to
         
@@ -96,8 +119,7 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 1) {
             self.view.layoutIfNeeded()
             self.titleLabel.transform = clicked ?
-                CGAffineTransform(scaleX: 2, y: 2)
-                    .concatenating(CGAffineTransform(translationX: 0, y: 100)) : .identity
+                CGAffineTransform(scaleX: 2, y: 2).concatenating(CGAffineTransform(translationX: 0, y: 100)) : .identity
         }
         
         
@@ -112,7 +134,7 @@ class ViewController: UIViewController {
         })
         
         if clicked {
-            self.yellowView.roundCorners([.bottomLeft, .bottomRight], radius: 200)
+            self.yellowView.roundCorners([.bottomLeft, .bottomRight], radius: self.yellowView.bounds.width / 2)
         }
     }
 }
@@ -124,6 +146,5 @@ extension UIView {
         let mask = CAShapeLayer()
         mask.path = path.cgPath
         self.layer.mask = mask
-        self.layer.masksToBounds = true
     }
 }
