3918c5306c6f59b16bfde2a3b201ae1319394fa9
yasin <yasin.kbas12@gmail.com>
2020-02-22 02:14:59 +0300

    add greater-less than support

--- UILab/Classes/Constraints/ConstraintTypes.swift
+++ UILab/Classes/Constraints/ConstraintTypes.swift
@@ -29,7 +29,7 @@ public struct Axises {
     static var empty: Axises {
         return Axises()
     }
-  
+    
     let top: YAxis?
     let left: XAxis?
     let bottom: YAxis?
@@ -67,57 +67,88 @@ internal struct ConstraintWrapper {
 }
 
 public enum ConstraintType {
-    case left               (XAxis,Inset = 0)
+    case left               (XAxis, Inset = 0)
     case leftOf             (View, Inset = 0)
+    case leftgte            (XAxis, Inset = 0)
+    case leftlte            (XAxis, Inset = 0)
     case right              (XAxis, Inset = 0)
     case rightOf            (View, Inset = 0)
+    case rightgte           (XAxis, Inset = 0)
+    case rightlte           (XAxis, Inset = 0)
     case top                (YAxis, Inset = 0)
     case topOf              (View, Inset = 0)
+    case topgte             (YAxis, Inset = 0)
+    case toplte             (YAxis, Inset = 0)
     case bottom             (YAxis, Inset = 0)
     case bottomOf           (View, Inset = 0)
+    case bottomgte          (YAxis, Inset = 0)
+    case bottomlte          (YAxis, Inset = 0)
     case leading            (XAxis, Inset = 0)
     case leadingOf          (View, Inset = 0)
+    case leadinggte         (XAxis, Inset = 0)
+    case leadinglte         (XAxis, Inset = 0)
     case trailing           (XAxis, Inset = 0)
     case trailingOf         (View, Inset = 0)
+    case trailinggte        (XAxis, Inset = 0)
+    case trailinglte        (XAxis, Inset = 0)
     case width              (Constant)
     case widthOf            (View, Inset = 0)
+    case widthgte           (Constant)
+    case widthlte           (Constant)
     case height             (Constant)
     case heightOf           (View, Inset = 0)
+    case heightgte          (Constant)
+    case heightlte          (Constant)
     case centerX            (XAxis, Inset = 0)
     case centerXOf          (View, Inset = 0)
     case centerY            (YAxis, Inset = 0)
     case centerYOf          (View, Inset = 0)
     case center             (View, x: Inset = 0, y: Inset = 0)
-    case widthDimension     (Dimension, Multiple = 1, Inset = 0)
-    case heightDimension    (Dimension, Multiple = 1, Inset = 0)
-    
+    case widthMultiple      (Dimension, Multiple = 1, Inset = 0)
+    case heightMultiple    (Dimension, Multiple = 1, Inset = 0)
     
     func constraint(_ view: UIView) -> Constraints{
         switch self {
         case .left(let anchor, let inset):              return [view.left.constraint(equalTo: anchor, constant: inset)]
         case .leftOf(let anchorView, let inset):        return [view.left.constraint(equalTo: anchorView.left, constant: inset)]
+        case .leftgte(let anchor, let inset):           return [view.left.constraint(greaterThanOrEqualTo: anchor, constant: inset)]
+        case .leftlte(let anchor, let inset):           return [view.left.constraint(lessThanOrEqualTo: anchor, constant: inset)]
         case .right(let anchor, let inset):             return [view.right.constraint(equalTo: anchor, constant: -inset)]
         case .rightOf(let anchorView, let inset):       return [view.right.constraint(equalTo: anchorView.right, constant: -inset)]
+        case .rightgte(let anchor, let inset):          return [view.right.constraint(greaterThanOrEqualTo: anchor, constant: inset)]
+        case .rightlte(let anchor, let inset):          return [view.right.constraint(lessThanOrEqualTo: anchor, constant: inset)]
         case .top(let anchor, let inset):               return [view.top.constraint(equalTo: anchor, constant: inset)]
         case .topOf(let anchorView, let inset):         return [view.top.constraint(equalTo: anchorView.top, constant: inset)]
+        case .topgte(let anchor, let inset):            return [view.top.constraint(greaterThanOrEqualTo: anchor, constant: inset)]
+        case .toplte(let anchor, let inset):            return [view.top.constraint(lessThanOrEqualTo: anchor, constant: inset)]
         case .bottom(let anchor, let inset):            return [view.bottom.constraint(equalTo: anchor, constant: -inset)]
         case .bottomOf(let anchorView, let inset):      return [view.bottom.constraint(equalTo: anchorView.bottom, constant: -inset)]
+        case .bottomgte(let anchor, let inset):         return [view.bottom.constraint(greaterThanOrEqualTo: anchor, constant: inset)]
+        case .bottomlte(let anchor, let inset):         return [view.bottom.constraint(lessThanOrEqualTo: anchor, constant: inset)]
         case .leading(let anchor, let inset):           return [view.leading.constraint(equalTo: anchor, constant: inset)]
         case .leadingOf(let anchorView, let inset):     return [view.leading.constraint(equalTo: anchorView.leading, constant: inset)]
+        case .leadinggte(let anchor, let inset):        return [view.leading.constraint(greaterThanOrEqualTo: anchor, constant: inset)]
+        case .leadinglte(let anchor, let inset):        return [view.leading.constraint(lessThanOrEqualTo: anchor, constant: inset)]
         case .trailing(let anchor, let inset):          return [view.trailing.constraint(equalTo: anchor, constant: inset)]
         case .trailingOf(let anchorView, let inset):    return [view.trailing.constraint(equalTo: anchorView.trailing, constant: inset)]
+        case .trailinggte(let anchor, let inset):       return [view.trailing.constraint(greaterThanOrEqualTo: anchor, constant: inset)]
+        case .trailinglte(let anchor, let inset):       return [view.trailing.constraint(lessThanOrEqualTo: anchor, constant: inset)]
         case .width(let constant):                      return [view.width.constraint(equalToConstant: constant)]
         case .widthOf(let anchorView, let inset):       return [view.width.constraint(equalTo: anchorView.width, constant: inset)]
+        case .widthgte(let constant):                   return [view.width.constraint(greaterThanOrEqualToConstant: constant)]
+        case .widthlte(let constant):                   return [view.width.constraint(lessThanOrEqualToConstant: constant)]
         case .height(let constant):                     return [view.height.constraint(equalToConstant: constant)]
         case .heightOf(let anchorView, let inset):      return [view.height.constraint(equalTo: anchorView.height, constant: inset)]
+        case .heightgte(let constant):                  return [view.height.constraint(greaterThanOrEqualToConstant: constant)]
+        case .heightlte(let constant):                  return [view.height.constraint(lessThanOrEqualToConstant: constant)]
         case .centerX(let anchor, let inset):           return [view.centerX.constraint(equalTo: anchor, constant: inset)]
         case .centerXOf(let anchorView, let inset):     return [view.centerX.constraint(equalTo: anchorView.centerX, constant: inset)]
         case .centerY(let anchor, let inset):           return [view.centerY.constraint(equalTo: anchor, constant: inset)]
         case .centerYOf(let anchorView, let inset):     return [view.centerY.constraint(equalTo: anchorView.centerY, constant: inset)]
-        
-        case .widthDimension(let dimension, let multiple, let inset):
+            
+        case .widthMultiple(let dimension, let multiple, let inset):
             return [view.width.constraint(equalTo: dimension, multiplier: multiple, constant: inset)]
-        case .heightDimension(let dimension, let multiple, let inset):
+        case .heightMultiple(let dimension, let multiple, let inset):
             return [view.height.constraint(equalTo: dimension, multiplier: multiple, constant: inset)]
         case .center(let anchorView, x: let xInset, y: let yInset):
             return [view.centerX.constraint(equalTo: anchorView.centerX, constant: xInset),
